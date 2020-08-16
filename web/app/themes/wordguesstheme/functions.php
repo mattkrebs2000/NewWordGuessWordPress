57ef9cbb12f52d1daac7c2a196d1a672e9d6944d
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-08-16 14:07:02 -0400

    new

--- web/app/themes/wordguesstheme/functions.php
+++ web/app/themes/wordguesstheme/functions.php
@@ -101,237 +101,236 @@ function addNFL() {
 
 function ImagesOfTeams() {
 
-    console.log("teamname ",
-        teamNameAssignment, victory, "
-        number ", teamNameAssignment.length)
+    console.log("teamname " +
+        teamNameAssignment + victory + teamNameAssignment.length);
 
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "bears") {
-            jQuery("#try").addClass("bears")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "bears") {
+        jQuery("#try").addClass("bears")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "browns") {
-            jQuery("#try").addClass("browns")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "browns") {
+        jQuery("#try").addClass("browns")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "bengals") {
-            jQuery("#try").addClass("bengals")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "bengals") {
+        jQuery("#try").addClass("bengals")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "bills") {
-            jQuery("#try").addClass("bills")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "bills") {
+        jQuery("#try").addClass("bills")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "broncos") {
-            jQuery("#try").addClass("broncos")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "broncos") {
+        jQuery("#try").addClass("broncos")
+    };
 
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "buccaneers") {
-            jQuery("#try").addClass("buccaneers")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "buccaneers") {
+        jQuery("#try").addClass("buccaneers")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "cardinals") {
-            jQuery("#try").addClass("cardinals")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "cardinals") {
+        jQuery("#try").addClass("cardinals")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "chargers") {
-            jQuery("#try").addClass("chargers")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "chargers") {
+        jQuery("#try").addClass("chargers")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "chiefs") {
-            jQuery("#try").addClass("chiefs")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "chiefs") {
+        jQuery("#try").addClass("chiefs")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "colts") {
-            jQuery("#try").addClass("colts")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "colts") {
+        jQuery("#try").addClass("colts")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "cowboys") {
-            jQuery("#try").addClass("cowboys")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "cowboys") {
+        jQuery("#try").addClass("cowboys")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "dolphins") {
-            jQuery("#try").addClass("dolphins")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "dolphins") {
+        jQuery("#try").addClass("dolphins")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "eagles") {
-            jQuery("#try").addClass("eagles")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "eagles") {
+        jQuery("#try").addClass("eagles")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "falcons") {
-            jQuery("#try").addClass("falcons")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "falcons") {
+        jQuery("#try").addClass("falcons")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "fortyniners") {
-            jQuery("#try").addClass("fortyniners")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "fortyniners") {
+        jQuery("#try").addClass("fortyniners")
+    };
 
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "giants") {
-            jQuery("#try").addClass("giants")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "giants") {
+        jQuery("#try").addClass("giants")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "jaguars") {
-            jQuery("#try").addClass("jaguars")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "jaguars") {
+        jQuery("#try").addClass("jaguars")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "jets") {
-            jQuery("#try").addClass("jets")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "jets") {
+        jQuery("#try").addClass("jets")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "lions") {
-            jQuery("#try").addClass("lions")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "lions") {
+        jQuery("#try").addClass("lions")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "packers") {
-            jQuery("#try").addClass("packers")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "packers") {
+        jQuery("#try").addClass("packers")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "panthers") {
-            jQuery("#try").addClass("panthers")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "panthers") {
+        jQuery("#try").addClass("panthers")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "patriots") {
-            jQuery("#try").addClass("patriots")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "patriots") {
+        jQuery("#try").addClass("patriots")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "raiders") {
-            jQuery("#try").addClass("raiders")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "raiders") {
+        jQuery("#try").addClass("raiders")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "rams") {
-            jQuery("#try").addClass("rams")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "rams") {
+        jQuery("#try").addClass("rams")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "ravens") {
-            jQuery("#try").addClass("ravens")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "ravens") {
+        jQuery("#try").addClass("ravens")
+    };
 
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "redskins") {
-            jQuery("#try").addClass("redskins")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "redskins") {
+        jQuery("#try").addClass("redskins")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "saints") {
-            jQuery("#try").addClass("saints")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "saints") {
+        jQuery("#try").addClass("saints")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "seahawks") {
-            jQuery("#try").addClass("seahawks")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "seahawks") {
+        jQuery("#try").addClass("seahawks")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "steelers") {
-            jQuery("#try").addClass("steelers")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "steelers") {
+        jQuery("#try").addClass("steelers")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "texans") {
-            jQuery("#try").addClass("texans")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "texans") {
+        jQuery("#try").addClass("texans")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "titans") {
-            jQuery("#try").addClass("titans")
-        };
+    if (victory === teamNameAssignment.length && teamNameAssignment === "titans") {
+        jQuery("#try").addClass("titans")
+    };
 
-        if (victory === teamNameAssignment.length && teamNameAssignment === "vikings") {
-            jQuery("#try").addClass("vikings")
+    if (victory === teamNameAssignment.length && teamNameAssignment === "vikings") {
+        jQuery("#try").addClass("vikings")
 
-        };
-    }
+    };
+}
 
-    ImagesOfTeams();
+ImagesOfTeams();
 
-    function newLetter() {
-        document.onkeyup = function(event) {
-            if (event.keyCode >= 65 && event.keyCode <= 90) {
-                userGuess = event.key.toLowerCase();
+function newLetter() {
+    document.onkeyup = function(event) {
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            userGuess = event.key.toLowerCase();
 
-                ImagesOfTeams()
+            ImagesOfTeams()
 
-                let doubles = listOfGuesses.includes(userGuess);
+            let doubles = listOfGuesses.includes(userGuess);
 
 
 
-                if (doubles === true) {
-                    alert("That letter has already been guessed. TRY AGAIN");
-                    return false;
-                }
-
-                for (var i = 0; i < teamNameAssignment.length; i++) {
-                    if (teamNameAssignment[i] === userGuess && doubles === false) {
-                        blankSpaces[i] = userGuess;
-                        victory++;
-                        listOfGuesses.push(userGuess);
-
-                    }
-                }
-                if (teamNameAssignment.indexOf(userGuess) == -1 && !listOfWrongGuesses.includes(" " + userGuess)) {
-                    listOfWrongGuesses.push(" " + userGuess);
-                    wrongGuesses--;
-                    wg = document.getElementById("firstGuess");
-                    wg.innerHTML = listOfWrongGuesses;
-
-                    ("wrong guesses", listOfWrongGuesses);
-                }
-
-
-                if (victory === teamNameAssignment.length) {
-
-
-
-                    alert(
-                        "Congratulations, You Won! Click the button below to play again."
-                    );
-
-                    games++;
-                    wins++;
-                    elem = document.getElementById("winTotal");
-                    elem.innerHTML = wins;
-
-                    percentage = (wins / games) * 100;
-                    roundedPercentage = Math.round(10 * percentage) / 10;
-
-                    eleme = document.getElementById("winningPercentage");
-                    eleme.innerHTML = roundedPercentage;
-
-                    document.getElementById("button").innerHTML =
-                        "Click Here To Play Again";
-
-                }
-
-                if (wrongGuesses === 0) {
-                    alert("You lost, click the button below to try again!");
-                    games++;
-                    losses++;
-                    elem = document.getElementById("lossTotal");
-                    elem.innerHTML = " " + losses;
-
-                    percentage = (wins / games) * 100;
-                    roundedPercentage = Math.round(10 * percentage) / 10;
-
-                    eleme = document.getElementById("winningPercentage");
-                    eleme.innerHTML = roundedPercentage;
-
-                    document.getElementById("try").style.backgroundImage =
-                        "url('./images/" + teamNameAssignment + ".png')";
-
-                    document.getElementById("button").innerHTML =
-                        "Click Here To Play Again";
-                }
-
-                el = document.getElementById("teamContainer");
-                el.innerHTML = blankSpaces.join(" ");
-
-                ele = document.getElementById("missedAttemtsLeft");
-                ele.innerHTML = " " + wrongGuesses;
-
-                return true;
-            } else {
-                alert("Please input alphabet characters only");
+            if (doubles === true) {
+                alert("That letter has already been guessed. TRY AGAIN");
                 return false;
             }
-        };
+
+            for (var i = 0; i < teamNameAssignment.length; i++) {
+                if (teamNameAssignment[i] === userGuess && doubles === false) {
+                    blankSpaces[i] = userGuess;
+                    victory++;
+                    listOfGuesses.push(userGuess);
+
+                }
+            }
+            if (teamNameAssignment.indexOf(userGuess) == -1 && !listOfWrongGuesses.includes(" " + userGuess)) {
+                listOfWrongGuesses.push(" " + userGuess);
+                wrongGuesses--;
+                wg = document.getElementById("firstGuess");
+                wg.innerHTML = listOfWrongGuesses;
+
+                ("wrong guesses", listOfWrongGuesses);
+            }
+
+
+            if (victory === teamNameAssignment.length) {
+
+
+
+                alert(
+                    "Congratulations, You Won! Click the button below to play again."
+                );
+
+                games++;
+                wins++;
+                elem = document.getElementById("winTotal");
+                elem.innerHTML = wins;
+
+                percentage = (wins / games) * 100;
+                roundedPercentage = Math.round(10 * percentage) / 10;
+
+                eleme = document.getElementById("winningPercentage");
+                eleme.innerHTML = roundedPercentage;
+
+                document.getElementById("button").innerHTML =
+                    "Click Here To Play Again";
+
+            }
+
+            if (wrongGuesses === 0) {
+                alert("You lost, click the button below to try again!");
+                games++;
+                losses++;
+                elem = document.getElementById("lossTotal");
+                elem.innerHTML = " " + losses;
+
+                percentage = (wins / games) * 100;
+                roundedPercentage = Math.round(10 * percentage) / 10;
+
+                eleme = document.getElementById("winningPercentage");
+                eleme.innerHTML = roundedPercentage;
+
+                document.getElementById("try").style.backgroundImage =
+                    "url('./images/" + teamNameAssignment + ".png')";
+
+                document.getElementById("button").innerHTML =
+                    "Click Here To Play Again";
+            }
+
+            el = document.getElementById("teamContainer");
+            el.innerHTML = blankSpaces.join(" ");
+
+            ele = document.getElementById("missedAttemtsLeft");
+            ele.innerHTML = " " + wrongGuesses;
+
+            return true;
+        } else {
+            alert("Please input alphabet characters only");
+            return false;
+        }
     };
+};
 </script>
 <?php
 }
